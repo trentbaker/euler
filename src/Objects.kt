0d1c8648968ada0a7dfd3c33d855fbb6467a2f70
trent <b.trent5@gmail.com>
2019-12-14 13:21:21 -0700

    oof

--- src/Objects.kt
+++ src/Objects.kt
@@ -1,5 +1,3 @@
-import java.lang.Exception
-
 class BinaryNode(var value: Int): Comparable<BinaryNode> {
     var l: BinaryNode? = null
     var r: BinaryNode? = null
@@ -10,31 +8,4 @@ class BinaryNode(var value: Int): Comparable<BinaryNode> {
     fun eatLargestLivingChild() {
         value += listOfNotNull(l, r).maxBy { it.value }?.value ?: 0
     }
-}
-
-class Ring<T>(private val values: List<T>, private val weight: Int = 1) {
-    init {
-        assert(values.isNotEmpty()) { throw Exception("must have values") }
-        assert(weight > 0) { throw Exception("must have weight > 0") }
-    }
-
-    override fun toString() = values.toString()
-
-    private var count = 0
-
-    fun current() = values[count / weight]
-
-    fun inc(i: Int = 1): Ring<T> {
-        count += i
-        if ((count / weight) > values.lastIndex) count %= values.size
-        return this
-    }
-}
-
-class Lexicon<T>(private val tumblers: List<Ring<T>>) {
-    override fun toString() = current().toString()
-    fun current() = tumblers.map { it.current() }
-    private fun inc(i: Int) = Lexicon(tumblers.map { it.inc(i) })
-
-    fun get(index: Int) = inc(index).current()
 }
